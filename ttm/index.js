608f6442b82bd77be5b1ef6b5e799f0e6445d6bf
owozsh <vbs.owozsh@gmail.com>
2021-01-30 16:45:55 -0300

    add index.js and keybinding

---
+++ ttm/index.js
@@ -0,0 +1,13 @@
+document.onkeyup = (event) => {
+    switch (event.key) {
+        case 'A': ; break; //--> New Scheduled Item
+        case 'a': ; break; //--> New Item
+        case 'n': ; break; //--> New Column
+        case 'e': ; break; //--> Edit Item
+        case 'x': ; break; //--> Delete Item
+        case 'j': ; break; //--> Item Down
+        case 'k': ; break; //--> Item Up
+        case 'h': ; break; //--> Column Left
+        case 'l': ; break; //--> Column Right
+    }
+}
