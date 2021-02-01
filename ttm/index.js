68dc0e1fd7913a5eac0712a6f1da70fc58c5bbff
owozsh <vbs.owozsh@gmail.com>
2021-02-01 16:27:51 -0300

    fix selection and add swap function

--- ttm/index.js
+++ ttm/index.js
@@ -12,38 +12,42 @@ const createElementExt = (element_type, classes) => {
     return element;
 }
 
-const select = (column, item) => {
-    if (column < columns.length && column >= 0) {
-        selected_column = column;
-        column_headers.forEach(element => element.classList.remove('column_selected'));
-        column_headers[selected_column].classList.add('column_selected');
-    }
-    if (item < items.length && item >= 0) {
-        selected_item = item;
-        items.forEach(element => element.classList.remove('item_selected'));
-        items[selected_item].classList.add('item_selected');
-    }
-}
-
-const update_lists = () => {
+const updateLists = () => {
     columns = document.querySelectorAll('.column');
     column_headers = document.querySelectorAll('.column_header');
     items = document.querySelectorAll('.item');
 }
 
+const select = (column, item) => {
+
+    if (column < columns.length && column >= 0) {
+        selected_column = column;
+        column_headers.forEach(element => element.classList.remove('column_selected'));
+        column_headers[selected_column].classList.add('column_selected');
+    }
+
+    if (item == 0) item++;
+    const column_items = columns[selected_column].children;
+    if (item < column_items.length && item > 0) {
+        selected_item = item;
+        items.forEach(element => element.classList.remove('item_selected'));
+        column_items[selected_item].classList.add('item_selected');
+    }
+}
+
 //___ADD
-const add_column = () => {
+const addColumn = () => {
     const column = createElementExt('div', ['column']);
     view.appendChild(column);
     const column_header = createElementExt('h2', ['column_header']);
     column_header.textContent = prompt('Enter the new column\'s header');
     column.appendChild(column_header);
 
-    update_lists();
+    updateLists();
     select(columns.length - 1, 0);
 }
 
-const add_item = (schedule) => {
+const addItem = (schedule) => {
     const item = createElementExt('div', ['item']);
     const content = prompt('Enter the new item\'s content');
     item.textContent = content;
@@ -56,25 +60,48 @@ const add_item = (schedule) => {
         item.prepend(schedule_element);
     }
 
-    update_lists();
-    select(selected_column, items.length - 1);
+    updateLists();
+    select(selected_column, columns[selected_column].children.length - 1);
+}
+
+//___MOVE ITEM
+const swap = (direction) => {
+    if (direction == 0 && selected_item + 1 < columns[selected_column].children.length) {
+        columns[selected_column].children[selected_item + 1].parentNode.insertBefore(columns[selected_column].children[selected_item + 1], columns[selected_column].children[selected_item]);
+        updateLists();
+        select(selected_column, selected_item + 1);
+    }
+
+    if (direction == 1 && selected_item - 1 > 0) {
+        columns[selected_column].children[selected_item].parentNode.insertBefore(columns[selected_column].children[selected_item], columns[selected_column].children[selected_item - 1]);
+        updateLists();
+        select(selected_column, selected_item - 1);
+    }
 }
 
 //___REMOVE
+const removeItem = () => {
+    if (columns[selected_column].children.length == 1) return;
+
+    columns[selected_column].removeChild(columns[selected_column].children[selected_item]);
+
+    updateLists();
+    select(selected_column, selected_item - 1);
+}
 
 document.onkeyup = (event) => {
     switch (event.key) {
-        case 'A': add_item(true); break; //--> New Scheduled Item
-        case 'a': add_item(false); break; //--> New Item
-        case 'n': add_column(); break; //--> New Column
+        case 'A': addItem(true); break; //--> New Scheduled Item
+        case 'a': addItem(false); break; //--> New Item
+        case 'n': addColumn(); break; //--> New Column
         case 'e': ; break; //--> Edit Item
-        case 'x': ; break; //--> Delete Item
-        case 'j': select(0, selected_item + 1); break; //--> Item Down
-        case 'k': select(0, selected_item - 1); break; //--> Item Up
+        case 'd': removeItem(); break; //--> Delete Item
+        case 'j': select(selected_column, selected_item + 1); break; //--> Item Down
+        case 'k': select(selected_column, selected_item - 1); break; //--> Item Up
         case 'l': select(selected_column + 1, 0); break; //--> Column Right
         case 'h': select(selected_column - 1, 0); break; //--> Column Left
-        case 'J': ; break; //--> Move Item Down
-        case 'K': ; break; //--> Move Item Up
+        case 'J': swap(0); break; //--> Move Item Down
+        case 'K': swap(1); break; //--> Move Item Up
         case 'H': ; break; //--> Move Column Left
         case 'L': ; break; //--> Move Column Right
     }
